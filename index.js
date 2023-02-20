0d878ce10319fc1432f0d24891869fc8da5dd634
Swati Tanu <swatitani01@gmail.com>
2023-02-20 22:46:30 +0530

    updated

---
+++ index.js
@@ -0,0 +1,43 @@
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+//Using Promises
+//const generateJokes = () => {
+    // const setHeader = {
+    //     headers: {
+    //         Accept : "application/json"
+    //     }
+    // }
+
+    // fetch("https://icanhazdadjoke.com", setHeader)
+    // .then((res) => res.json())
+    // .then((data) => {
+    //     jokes.innerHTML = data.joke;
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // })
+//}
+
+//Using Async Await
+const generateJokes = async () => {
+try{
+    const setHeader = {
+        headers: {
+            Accept : "application/json"
+        }
+    }
+
+const res = await fetch("https://icanhazdadjoke.com", setHeader);
+
+const data = await res.json();
+jokes.innerHTML = data.joke;
+}
+catch(err){
+        console.log(`The error is ${err}`);
+}
+}
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
+
